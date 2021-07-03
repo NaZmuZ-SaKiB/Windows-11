e85059d453708699c1ac3eaeb055c04e8f1f7a93
NaZmuZ-SaKiB <sakibnazmuz01@gmail.com>
2021-07-03 21:33:01 +0600

    added new settings in file explorer

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const startMenu = document.getElementById('start-menu')
 const searchBar = document.getElementById('search-bar')
 const fileExplorerIcon = document.getElementById('file-explorer-icon')
 const fileExplorer = document.getElementById('file-explorer')
+const fileExplorerImg = document.querySelector('#file-explorer img')
 const fileExplorerClose = document.getElementById('file-explorar-close')
+const fileExplorerMax = document.getElementById('file-explorar-max')
+const fileExplorerNormal = document.getElementById('file-explorar-normal')
+const fileExplorerMin = document.getElementById('file-explorar-min')
 const fileIconStart = document.getElementById('file-start')
 
 
@@ -39,4 +43,36 @@ fileExplorerIcon.addEventListener('click',() => {
 fileExplorerClose.addEventListener('click', () => {
     fileExplorer.classList.remove('file-explorer-open')
     fileIconStart.style.display = "none"
+})
+fileExplorerMax.addEventListener('click', () => {
+    fileExplorerImg.src = 'img/file-explorer-max.png'
+    fileExplorer.classList.add('file-explorer-max-open')
+    
+})
+
+fileExplorerNormal.addEventListener('click', () => {
+    fileExplorerImg.src = 'img/file-explorer.png'
+    fileExplorer.classList.remove('file-explorer-max-open')
+    fileExplorer.classList.add('file-explorer-open')
+})
+
+fileExplorerMin.addEventListener('click', () => {
+    fileExplorer.style.top = '100vh'
+    fileExplorer.style.transform = 'translate(-50%, 0) scale(.2)'
+})
+fileIconStart.addEventListener('click', () => {
+    if(fileExplorer.hasAttribute('style') && fileExplorerImg.getAttribute('src') === 'img/file-explorer.png'){
+        fileExplorer.removeAttribute('style')
+    }
+    else if(fileExplorerImg.getAttribute('src') === 'img/file-explorer.png'){
+        fileExplorer.style.top = '100vh'
+        fileExplorer.style.transform = 'translate(-50%, 0) scale(.2)'
+    }
+    else if(fileExplorer.hasAttribute('style') && fileExplorerImg.getAttribute('src') === 'img/file-explorer-max.png'){
+        fileExplorer.removeAttribute('style')
+    }
+    else if(fileExplorerImg.getAttribute('src') === 'img/file-explorer-max.png'){
+        fileExplorer.style.top = '100vh'
+        fileExplorer.style.transform = 'translate(-50%, 0) scale(.2)'
+    }
 })
