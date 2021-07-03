7b49ff13dd22130ead9a2ec71fff7ba0603f3fae
NaZmuZ-SaKiB <sakibnazmuz01@gmail.com>
2021-07-03 18:53:18 +0600

    adding files icon

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ const searchBar = document.getElementById('search-bar')
 const fileExplorerIcon = document.getElementById('file-explorer-icon')
 const fileExplorer = document.getElementById('file-explorer')
 const fileExplorerClose = document.getElementById('file-explorar-close')
-
-
+const fileIconStart = document.getElementById('file-start')
 
 
 startIcon.addEventListener('click', () => {
@@ -20,23 +19,24 @@ searchIcon.addEventListener('click', () => {
     if(startMenu.classList.contains('start-menu-animation')){
         startMenu.classList.remove('start-menu-animation')
     }
-    if(searchBar.classList.contains('search-bar-animation')){
-        searchBar.classList.remove('search-bar-animation')
-
-    }else{
-        searchBar.classList.add('search-bar-animation')
-        
-    }
-    // searchBar.classList.toggle('search-bar-animation')
+    searchBar.classList.toggle('search-bar-animation')
 
 })
 fileExplorerIcon.addEventListener('click',() => {
     if(searchBar.classList.contains('search-bar-animation')){
         searchBar.classList.remove('search-bar-animation')
     }
-    fileExplorer.classList.toggle('file-explorer-open') 
+    if(fileExplorer.classList.contains('file-explorer-open')){
+        fileExplorer.classList.remove('file-explorer-open')
+        fileIconStart.style.display = "none"
+    }else{
+        fileExplorer.classList.add('file-explorer-open')
+        fileIconStart.style.display = "inline-block"
+    }
+
 })
 
 fileExplorerClose.addEventListener('click', () => {
     fileExplorer.classList.remove('file-explorer-open')
+    fileIconStart.style.display = "none"
 })
