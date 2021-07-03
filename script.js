5a15db82cb0fe5b68c75ef8142401f53647812f8
NaZmuZ-SaKiB <sakibnazmuz01@gmail.com>
2021-07-03 23:10:44 +0600

    almost final vertion i think

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const startIcon = document.getElementById('start-icon')
 const searchIcon = document.getElementById('search-icon')
 const startMenu = document.getElementById('start-menu')
 const searchBar = document.getElementById('search-bar')
-const fileExplorerIcon = document.getElementById('file-explorer-icon')
+const fileExplorerIcon = document.querySelectorAll('#file-explorer-icon')
 const fileExplorer = document.getElementById('file-explorer')
 const fileExplorerImg = document.querySelector('#file-explorer img')
 const fileExplorerClose = document.getElementById('file-explorar-close')
@@ -26,22 +26,30 @@ searchIcon.addEventListener('click', () => {
     searchBar.classList.toggle('search-bar-animation')
 
 })
-fileExplorerIcon.addEventListener('click',() => {
-    if(searchBar.classList.contains('search-bar-animation')){
-        searchBar.classList.remove('search-bar-animation')
-    }
-    if(fileExplorer.classList.contains('file-explorer-open')){
-        fileExplorer.classList.remove('file-explorer-open')
-        fileIconStart.style.display = "none"
-    }else{
-        fileExplorer.classList.add('file-explorer-open')
-        fileIconStart.style.display = "inline-block"
-    }
-
+fileExplorerIcon.forEach(icon => {
+    icon.addEventListener('click',() => {
+        if(searchBar.classList.contains('search-bar-animation')){
+            searchBar.classList.remove('search-bar-animation')
+        }
+        fileExplorerImg.src = 'img/file-explorer.png'
+        if(startMenu.classList.contains('start-menu-animation')){
+            startMenu.classList.remove('start-menu-animation')
+        }
+        if(fileExplorer.classList.contains('file-explorer-open')){
+            fileExplorer.classList.remove('file-explorer-open')
+            fileIconStart.style.display = "none"
+        }else{
+            fileExplorer.classList.add('file-explorer-open')
+            fileIconStart.style.display = "inline-block"
+        }
+    
+    })
 })
+
 
 fileExplorerClose.addEventListener('click', () => {
     fileExplorer.classList.remove('file-explorer-open')
+    fileExplorer.classList.remove('file-explorer-max-open')
     fileIconStart.style.display = "none"
 })
 fileExplorerMax.addEventListener('click', () => {
